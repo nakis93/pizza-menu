905853d16c256ccf93c71dae222f75e3ed842a95
nakis93 <zjnakis@gmail.com>
2025-07-09 15:38:03 +0300

    feat: create a dumb number-input component

---
+++ src/app/shared/components/number-input/number-input.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-number-input',
+  imports: [],
+  templateUrl: './number-input.component.html',
+  styleUrl: './number-input.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: NumberInputComponent,
+      multi: true
+    }
+  ]
+})
+export class NumberInputComponent {
+  @Input() step = 0.01;
+  @Input() min = 0;
+  @Input() max = 9999;
+  value: number = 0;
+  disabled = false;
+
+  onChange = (_: any) => { };
+  onTouched = () => { };
+
+  writeValue(value: number): void {
+    this.value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
+  }
+
+  onInputChange(event: Event) {
+    const input = (event.target as HTMLInputElement).value;
+    const parsed = parseFloat(input);
+    const safeValue = isNaN(parsed) ? 0 : Math.max(this.min, Math.min(parsed, this.max));
+    this.value = safeValue;
+    this.onChange(safeValue);
+    this.onTouched();
+  }
+}
