6a0954443b73f0b9a600f4283bf5be72300f1828
nakis93 <zjnakis@gmail.com>
2025-07-09 11:32:40 +0300

    feat: simulate backend API for data fetching

---
+++ src/app/constants/fakeData/items.ts
@@ -0,0 +1,12 @@
+import { Item } from "../../models/item";
+
+export const items: Item[] = [
+    {
+        itemId: 0,
+        name: 'Margherita'
+    },
+    {
+        itemId: 1,
+        name: 'Pepperoni'
+    },
+];
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { itemSizes } from '../constants/fakeData/sizes';
+import { itemPrices } from '../constants/fakeData/prices';
+import { items } from '../constants/fakeData/items';
+import { Observable, of } from 'rxjs';
+import { Item } from '../models/item';
+import { Price } from '../models/price';
+import { Size } from '../models/size';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  itemSizes = itemSizes;
+  prices = itemPrices;
+  items = items;
+  constructor() { }
+
+  getItems(): Observable<Item[]> {
+    return of(items);
+  }
+
+  getPrices(): Observable<Price[]> {
+    return of(itemPrices);
+  }
+
+  getSizes(): Observable<Size[]> {
+    return of(itemSizes);
+  }
+
+}
